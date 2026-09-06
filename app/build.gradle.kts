00d2ec46aca5aac153e0e04a2c4f7c2fd20cc14c
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-07 06:31:16 +0900

    Use monotonic default versionCode for v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 val airKeystore = file("aircontrol.keystore")
 val appVersionName = providers.gradleProperty("VERSION_NAME").orElse("0.1.3")
-val appVersionCode = providers.gradleProperty("VERSION_CODE").orElse("4")
+val appVersionCode = providers.gradleProperty("VERSION_CODE").orElse("103")
 
 android {
     namespace = "com.ikegami99.aircontrol"
